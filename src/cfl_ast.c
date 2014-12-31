3bdab688419ca1ffa4ace19fd05d009638fb5c53
Anthony Burzillo <burz@jhu.edu>
2014-12-31 05:45:17 -0500

    Add node creation functions

--- src/cfl_ast.c
+++ src/cfl_ast.c
@@ -2,6 +2,72 @@
 
 #include <stdlib.h>
 
+int cfl_create_node_bool(cfl_node* node, bool value)
+{
+    node->number_of_children = 0;
+    node->data = malloc(sizeof(bool));
+
+    if(!node->data)
+        return 0;
+
+    return 1;
+}
+
+int cfl_create_node_variable(cfl_node* node, const char* string)
+{
+    node->number_of_children = 0;
+    node->data = malloc(sizeof(char) * MAX_IDENTIFIER_LENGTH);
+
+    if(!node->data)
+        return 0;
+
+    return 1;
+}
+
+int cfl_create_node_and(cfl_node* node, cfl_node* left, cfl_node* right)
+{
+    node->number_of_children = 2;
+    node->data = 0;
+    node->children = malloc(sizeof(cfl_node*) * 2);
+
+    if(!node->children)
+        return 0;
+
+    node->children[0] = left;
+    node->children[0] = right;
+
+    return 1;
+}
+
+int cfl_create_node_or(cfl_node* node, cfl_node* left, cfl_node* right)
+{
+    node->number_of_children = 2;
+    node->data = 0;
+    node->children = malloc(sizeof(cfl_node*) * 2);
+
+    if(!node->children)
+        return 0;
+
+    node->children[0] = left;
+    node->children[0] = right;
+
+    return 1;
+}
+
+int cfl_create_node_not(cfl_node* node, cfl_node* child)
+{
+    node->number_of_children = 1;
+    node->data = 0;
+    node->children = malloc(sizeof(cfl_node*));
+
+    if(!node->children)
+        return 0;
+
+    *node->children = child;
+
+    return 1;
+}
+
 void cfl_delete_node(cfl_node* node)
 {
     if(node->data)
@@ -11,10 +77,10 @@ void cfl_delete_node(cfl_node* node)
 
     for( ; i < node->number_of_children; ++i)
     {
-        cfl_delete_node(&node->children[i]);
+        cfl_delete_node(node->children[i]);
 
-        free(node);
+        free(node->children[i]);
     }
 
-    node->type = CFL_NODE_NULL;
+    free(node->children);
 }
--- src/cfl_ast.h
+++ src/cfl_ast.h
@@ -1,8 +1,11 @@
 #ifndef _CFL_AST_H_
 #define _CFL_AST_H_
 
+#include <stdbool.h>
+
+#define MAX_IDENTIFIER_LENGTH 100
+
 typedef enum {
-    CFL_NODE_NULL = 0,
     CFL_NODE_BOOL,
     CFL_NODE_VARIABLE,
     CFL_NODE_AND,
@@ -11,11 +14,18 @@ typedef enum {
 } cfl_node_type;
 
 typedef struct cfl_node_t {
+    int line_number;
     cfl_node_type type;
-    void* data;
     int number_of_children;
-    struct cfl_node_t *children;
+    void* data;
+    struct cfl_node_t** children;
 } cfl_node;
+
+int cfl_create_node_bool(cfl_node* node, bool value);
+int cfl_create_node_variable(cfl_node* node, const char* string);
+int cfl_create_node_and(cfl_node* node, cfl_node* left, cfl_node* right);
+int cfl_create_node_or(cfl_node* node, cfl_node* left, cfl_node* right);
+int cfl_create_node_not(cfl_node* node, cfl_node* child);
 
 void cfl_delete_node(cfl_node* node);
 
