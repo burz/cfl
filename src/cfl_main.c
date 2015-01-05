4f9d1932f047f259b7722a5ebc9d604f6a223001
Anthony Burzillo <burz@jhu.edu>
2015-01-05 07:11:41 -0500

    Flesh out main

--- src/cfl_main.c
+++ src/cfl_main.c
@@ -3,55 +3,87 @@
 #include "cfl_eval.h"
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+
+static char usage[] = "USAGE: cfl filename\n"
+                      "           -ast filename\n"
+                      "           -typecheck filename";
 
 int main(int argc, char* argv[])
 {
-    char* file;
+    if(argc < 2)
+    {
+        fprintf(stderr, "%s\n", usage);
 
-    if(argc > 1)
-        file = argv[1];
-    else
-        file = "lol.cfl";
+        return 1;
+    }
 
     cfl_node node;
 
-    if(!cfl_parse_file(&node, file))
-        return 1;
-
-    cfl_print_node(&node);
-
-    printf("\n$$$$$$$$$$$$$$$$$$$$$$$$$$"
-           "\n          TYPE            "
-           "\n$$$$$$$$$$$$$$$$$$$$$$$$$$\n\n");
-
-    cfl_type* type = cfl_typecheck(&node);
-
-    if(!type)
+    if(argc > 2)
     {
+        if(!strcmp(argv[1], "-ast"))
+        {
+            if(!cfl_parse_file(&node, argv[2]))
+                return 1;
+
+            cfl_print_node(&node);
+
+            cfl_delete_node(&node);
+        }
+        else if(argc > 2 && !strcmp(argv[1], "-typecheck"))
+        {
+            if(!cfl_parse_file(&node, argv[2]))
+                return 1;
+
+            cfl_type* type = cfl_typecheck(&node);
+
+            if(!type)
+            {
+                cfl_delete_node(&node);
+
+                return 1;
+            }
+
+            cfl_print_type(type);
+        }
+        else
+        {
+            fprintf(stderr, "ERROR: unrecognized option \"%s\"\n%s\n",
+                            argv[1], usage);
+
+            return 1;
+        }
+
+        return 0;
+    }
+    else
+    {
+        if(!cfl_parse_file(&node, argv[1]))
+            return 1;
+
+        cfl_type* type = cfl_typecheck(&node);
+
+        if(!type)
+        {
+            cfl_delete_node(&node);
+
+            return 1;
+        }
+
+        cfl_free_type(type);
+
+        if(!cfl_evaluate(&node))
+        {
+            cfl_delete_node(&node);
+
+            return 1;
+        }
+
+        cfl_print_node(&node);
+
         cfl_delete_node(&node);
 
-        return 1;
+        return 0;
     }
-
-    cfl_print_type(type);
-
-    cfl_free_type(type);
-
-    printf("\n>>>>>>>>>>>>>>>>>>>>>>>>>>"
-           "\n         RESULT           "
-           "\n>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n");
-
-    if(!cfl_evaluate(&node))
-    {
-        cfl_delete_node(&node);
-
-        return 1;
-    }
-
-    cfl_print_node(&node);
-
-    cfl_delete_node(&node);
-
-    return 0;
 }
