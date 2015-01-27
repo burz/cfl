738f2cf36121b245f1374f93c6cb3d019d6a23a1
Anthony Burzillo <burz@jhu.edu>
2015-01-27 13:06:32 -0500

    Finish functions

--- include/cfl_typed_program.h
+++ include/cfl_typed_program.h
@@ -1,16 +1,9 @@
-#ifndef _CFL_PROGRAM_LLVM_H_
-#define _CFL_PROGRAM_LLVM_H_
+#ifndef _CFL_TYPED_PROGRAM_H_
+#define _CFL_TYPED_PROGRAM_H_
 
+#include "cfl_typed_program.types.h"
 #include "cfl_ast.h"
 #include "cfl_type.types.h"
-
-typedef struct cfl_typed_node_t {
-    cfl_node_type node_type;
-    cfl_type* resulting_type;
-    unsigned int number_of_children;
-    void* data;
-    struct cfl_typed_node_t** children;
-} cfl_typed_node;
 
 bool cfl_is_free_in_typed_node(char* name, cfl_typed_node* node);
 
@@ -22,25 +15,9 @@ cfl_typed_node* cfl_create_typed_node(cfl_node_type node_type,
 
 void cfl_free_typed_node(cfl_typed_node* node);
 
-typedef struct cfl_typed_node_list_t {
-    cfl_typed_node* node;
-    struct cfl_typed_node_list_t* next;
-} cfl_typed_node_list;
-
 void cfl_free_typed_node_list(cfl_typed_node_list* list);
 
-typedef struct cfl_typed_definition_list_t {
-    char* name;
-    cfl_typed_node* definition;
-    struct cfl_typed_definition_list_t* next;
-} cfl_typed_definition_list;
-
 void cfl_free_typed_definition_list(cfl_typed_definition_list* list);
-
-typedef struct {
-    cfl_typed_definition_list* definitions;
-    cfl_typed_node* main;
-} cfl_typed_program;
 
 void cfl_free_typed_program(cfl_typed_program* program);
 void cfl_print_typed_program(cfl_typed_program* program);
