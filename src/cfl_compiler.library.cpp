0b115e5d1c1a24e73da09e5028cbb571c8991ef4
Anthony Burzillo <burz@jhu.edu>
2015-06-11 14:41:17 -0400

    Fix random function in compilation

--- src/cfl_compiler.library.cpp
+++ src/cfl_compiler.library.cpp
@@ -29,6 +29,9 @@ llvm::Value* Compiler::create_random_function_struct(
     llvm::Function* cfl_rand_def =
         llvm::cast<llvm::Function>(top_module->getFunction("__cfl_rand"));
 
+    llvm::Function* cfl_rand_application_def =
+        llvm::cast<llvm::Function>(top_module->getFunction("__cfl_rand_application"));
+
     if(!cfl_rand_def)
     {
         std::vector<llvm::Type*> time_args;
@@ -98,46 +101,65 @@ llvm::Value* Compiler::create_random_function_struct(
 
         builder->CreateRet(result);
 
+        std::vector<llvm::Type*> cfl_rand_application_args;
+        cfl_rand_application_args.push_back(builder->getInt8PtrTy());
+        cfl_rand_application_args.push_back(builder->getInt8PtrTy());
+        cfl_rand_application_args.push_back(builder->getInt32Ty());
+        llvm::ArrayRef<llvm::Type*> cfl_rand_application_args_ref(cfl_rand_application_args);
+
+        llvm::FunctionType* cfl_rand_application_type =
+            llvm::FunctionType::get(builder->getInt32Ty(), cfl_rand_application_args_ref, false);
+
+        cfl_rand_application_def = llvm::Function::Create(
+            cfl_rand_application_type, llvm::Function::ExternalLinkage,
+            "__cfl_rand_application", top_module);
+
+        llvm::BasicBlock* application_function_entry = llvm::BasicBlock::Create(
+            global_context, "function_entry", cfl_rand_application_def);
+
+        builder->SetInsertPoint(application_function_entry);
+
+        llvm::Function::arg_iterator arg_itt = cfl_rand_application_def->arg_begin();
+
+        llvm::Value* function_pointer = arg_itt++;
+
+        llvm::Value* function = builder->CreatePointerCast(
+            function_pointer, cfl_rand_type->getPointerTo(), "function");
+
+        arg_itt++;
+
+        llvm::Value* argument = arg_itt++;
+
+        llvm::Value* applied_result = builder->CreateCall(function, argument, "applied_result");
+
+        builder->CreateRet(applied_result);
+
         builder->SetInsertPoint(entry_block);
     }
 
-    llvm::ArrayType* array_type =
-        llvm::ArrayType::get(builder->getInt8PtrTy(), 0);
-
     std::vector<llvm::Type*> members;
-    members.push_back(cfl_rand_def->getType());
-    members.push_back(array_type->getPointerTo());
+    members.push_back(cfl_rand_application_def->getType()->getPointerTo());
+    members.push_back(builder->getInt8PtrTy());
+    members.push_back(builder->getInt8PtrTy());
     llvm::ArrayRef<llvm::Type*> members_ref(members);
 
-    llvm::StructType* struct_type =
-        llvm::StructType::get(global_context, members_ref);
-
-    std::vector<llvm::Constant*> initial_values;
-    llvm::ArrayRef<llvm::Constant*> initial_values_ref(initial_values);
-
-    llvm::Constant* initial_arguments =
-        llvm::ConstantArray::get(array_type, initial_values_ref);
-
-    llvm::Constant* initial_array_pointer =
-        llvm::ConstantPointerNull::get(array_type->getPointerTo());
+    llvm::StructType* struct_type = llvm::StructType::get(global_context, members_ref);
 
     std::vector<llvm::Constant*> function_values;
-    function_values.push_back(cfl_rand_def);
-    function_values.push_back(initial_array_pointer);
+    function_values.push_back(cfl_rand_application_def);
+    function_values.push_back(
+        llvm::ConstantPointerNull::get(builder->getInt8PtrTy()));
+    function_values.push_back(
+        llvm::ConstantPointerNull::get(builder->getInt8PtrTy()));
     llvm::ArrayRef<llvm::Constant*> function_values_ref(function_values);
 
     llvm::Constant* initial_struct =
         llvm::ConstantStruct::get(struct_type, function_values_ref);
 
-    llvm::Value* arguments_pointer = call_malloc(array_type, parent, entry_block);
+    llvm::Value* cfl_rand_pointer =
+        builder->CreatePointerCast(cfl_rand_def, builder->getInt8PtrTy());
 
-    llvm::Value* arguments_space = builder->CreatePointerCast(
-        arguments_pointer, array_type->getPointerTo(), "arguments_space");
-
-    builder->CreateStore(initial_arguments, arguments_space);
-
-    return builder->CreateInsertValue(
-        initial_struct, arguments_space, 1, "random_function_struct");
+    return builder->CreateInsertValue(initial_struct, cfl_rand_pointer, 1, "function_struct");
 }
 
 } // end namespace Cfl
